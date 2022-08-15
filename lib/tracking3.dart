5fce74686c8d7847ee2c53a42a90724cf8b3d403
Arleen <16521172@mahasiswa.itb.ac.id>
2022-08-15 22:08:53 +0700

    Tracking -revisi font

--- lib/tracking3.dart
+++ lib/tracking3.dart
@@ -16,173 +16,215 @@ class Tracking3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Tracking'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                IconData(0xe09c,
-                    fontFamily: 'MaterialIcons', matchTextDirection: true),
-                color: Colors.white,
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Tracking4()),
-                );
-              },
-            )
-          ],
-        ),
         body: Stack(alignment: Alignment.center, children: <Widget>[
-          // profil petugas:
-          Positioned(
-              top: 5.0,
-              left: 10.0,
-              child: Align(
-                alignment: Alignment.center,
-                child: Container(
-                  height: 100.0,
-                  width: 340.0,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      color: Colors.orange[600]),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget>[
-                      ListTile(
-                        contentPadding: EdgeInsets.all(10.0),
-                        leading: CircleAvatar(
-                          radius: 25.0,
-                        ),
-                        title: Text("E-Teen",
-                            style: TextStyle(
-                                fontSize: 30.0, fontWeight: FontWeight.bold)),
-                        subtitle: Text(
-                          "+62 8XXXXXXXXXX",
-                          style: TextStyle(fontSize: 18.0),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )),
-          // tombol hubungi petugas:
-          Positioned(
-              top: 115.0,
-              left: 10.0,
-              child: SizedBox(
-                  height: 40.0,
-                  width: 340.0,
-                  child: new ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.indigo,
-                          side: BorderSide(width: 10.0, color: Colors.indigo)),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => HubungiPetugas()),
-                        );
-                      },
-                      child: const Text("Hubungi Petugas")))),
-          // tracking:
-          Positioned(
-              top: 160.0,
-              left: 10.0,
-              child: Container(
-                  height: 150.0,
-                  width: 340.0,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      color: Colors.blue[50]),
-                  padding: const EdgeInsets.all(5.0),
-                  child: Align(
-                      alignment: Alignment.center,
-                      child: new RichText(
-                          text: new TextSpan(
-                              style: new TextStyle(
-                                fontSize: 12.0,
-                                color: Colors.black,
-                                height: 1.8,
-                              ),
-                              children: <TextSpan>[
-                            new TextSpan(
-                                text: "Konfirmasi pembuangan sampah.\n"),
-                            new TextSpan(
-                              text: "Petugas sedang menuju lokasi Anda.\n",
-                            ),
-                            new TextSpan(
-                                text: "Petugas sampai di lokasi Anda.\n"),
-                            new TextSpan(
-                                text:
-                                    "Petugas sedang menuju pusat pengelolaan sampah.\n",
-                                style:
-                                    new TextStyle(fontWeight: FontWeight.bold)),
-                            new TextSpan(
-                                text:
-                                    "Petugas sampai di pusat pengelolaan sampah.\n"),
-                            new TextSpan(text: "Pembuangan sampah selesai!"),
-                          ]))))),
-          // gambar peta:
-          Positioned(
-            top: 320.0,
-            left: 10.0,
+      Positioned(
+          top: 45.0,
+          left: 60.0,
+          child: Text("Tracking",
+              style: TextStyle(
+                  fontFamily: "Nunito",
+                  fontSize: 30.0,
+                  color: Color.fromARGB(255, 31, 22, 86),
+                  fontWeight: FontWeight.w800))),
+      // tombol next:
+      Positioned(
+          top: 45.0,
+          left: 310.0,
+          child: Container(
+              //color: Colors.green,
+              child: IconButton(
+            icon: Icon(
+              IconData(0xe09c,
+                  fontFamily: 'MaterialIcons', matchTextDirection: true),
+              color: Color.fromARGB(255, 31, 22, 86),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Tracking4()),
+              );
+            },
+          ))),
+
+      // tombol back ke home:
+      Positioned(
+          top: 45.0,
+          left: 5.0,
+          child: Container(
+              //color: Colors.green,
+              child: IconButton(
+            icon: Icon(
+              IconData(0xe092,
+                  fontFamily: 'MaterialIcons', matchTextDirection: true),
+              color: Color.fromARGB(255, 31, 22, 86),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Home()),
+              );
+            },
+          ))),
+
+      // profil petugas:
+      Positioned(
+          top: 100.0,
+          left: 10.0,
+          child: Align(
+            alignment: Alignment.center,
             child: Container(
+              height: 92.0,
               width: 340.0,
-              height: 185.0,
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                  color: Colors.blue[50]),
-              child: const Image(
-                fit: BoxFit.cover,
-                image: AssetImage('images/map_zerotronics.png'),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Colors.orange[600]),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  ListTile(
+                    contentPadding: EdgeInsets.all(10.0),
+                    leading: CircleAvatar(
+                      radius: 20.0,
+                    ),
+                    title: Text("E-Teen",
+                        style: TextStyle(
+                            fontFamily: "Nunito",
+                            fontSize: 25.0,
+                            color: Color.fromARGB(255, 31, 22, 86),
+                            fontWeight: FontWeight.w800)),
+                    subtitle: Text(
+                      "+62 8XXXXXXXXXX",
+                      style: TextStyle(
+                          fontFamily: "Nunito",
+                          fontSize: 14.0,
+                          color: Color.fromARGB(255, 31, 22, 86)),
+                    ),
+                  ),
+                ],
               ),
             ),
+          )),
+      // tombol hubungi petugas:
+      Positioned(
+          top: 195.0,
+          left: 10.0,
+          child: SizedBox(
+              height: 40.0,
+              width: 340.0,
+              child: new ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.indigo,
+                      side: BorderSide(width: 10.0, color: Colors.indigo)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HubungiPetugas()),
+                    );
+                  },
+                  child: const Text("Hubungi Petugas",
+                      style: TextStyle(
+                          fontFamily: "Nunito",
+                          fontSize: 15.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800))))),
+      // tracking:
+      Positioned(
+          top: 240.0,
+          left: 10.0,
+          child: Container(
+              height: 150.0,
+              width: 340.0,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Colors.blue[50]),
+              padding: const EdgeInsets.all(5.0),
+              child: Align(
+                  alignment: Alignment.center,
+                  child: new RichText(
+                      text: new TextSpan(
+                          style: TextStyle(
+                              fontFamily: "Nunito",
+                              fontSize: 12.0,
+                              color: Color.fromARGB(255, 31, 22, 86),
+                              height: 1.8),
+                          children: <TextSpan>[
+                        new TextSpan(text: "Konfirmasi pembuangan sampah.\n"),
+                        new TextSpan(
+                            text: "Petugas sedang menuju lokasi Anda.\n"),
+                        new TextSpan(text: "Petugas sampai di lokasi Anda.\n"),
+                        new TextSpan(
+                            text:
+                                "Petugas sedang menuju pusat pengelolaan sampah.\n",
+                            style: new TextStyle(fontWeight: FontWeight.bold)),
+                        new TextSpan(
+                            text:
+                                "Petugas sampai di pusat pengelolaan sampah.\n"),
+                        new TextSpan(text: "Pembuangan sampah selesai!"),
+                      ]))))),
+      // gambar peta:
+      Positioned(
+        top: 400.0,
+        left: 10.0,
+        child: Container(
+          width: 340.0,
+          height: 185.0,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              color: Colors.blue[50]),
+          child: const Image(
+            fit: BoxFit.cover,
+            image: AssetImage('images/map_zerotronics.png'),
           ),
-          // tombol cancel:
-          Positioned(
-              top: 510.0,
-              left: 10.0,
-              child: SizedBox(
-                  height: 40.0,
-                  width: 340.0,
-                  child: new ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.red,
-                          side: BorderSide(width: 10.0, color: Colors.red)),
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: Text('Peringatan'),
-                                content: Text(
-                                    'Apakah Anda yakin ingin membatalkan pembuangan sampah?'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    child: Text('Tidak'),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                  ),
-                                  TextButton(
-                                    child: Text('Ya'),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => Home()),
-                                      );
-                                    },
-                                  ),
-                                ],
-                              );
-                            });
-                      },
-                      child: const Text("Batal Buang Sampah")))),
-        ]));
+        ),
+      ),
+      // tombol cancel:
+      Positioned(
+          top: 590.0,
+          left: 10.0,
+          child: SizedBox(
+              height: 40.0,
+              width: 340.0,
+              child: new ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                      side: BorderSide(width: 10.0, color: Colors.red)),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Peringatan'),
+                            content: Text(
+                                'Apakah Anda yakin ingin membatalkan pembuangan sampah?'),
+                            actions: <Widget>[
+                              TextButton(
+                                child: Text('Tidak'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              TextButton(
+                                child: Text('Ya'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Home()),
+                                  );
+                                },
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                  child: const Text("Batal Buang Sampah",
+                      style: TextStyle(
+                          fontFamily: "Nunito",
+                          fontSize: 15.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800))))),
+    ]));
   }
 }
